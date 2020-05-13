740c9e56e3507c79b21710ab80aa86e36952b685
Maurice <maurice@rlg.local>
2020-05-13 10:49:49 -0500

    Add moving logic to comets

--- canon-game/src/Comets.lua
+++ canon-game/src/Comets.lua
@@ -1,17 +1,32 @@
 Comet = Class{}
-
+local comet_speed = 2
 function Comet:init()
     self.comets = {}
     self.image = love.graphics.newImage('graphics/asteroid.png')
     for i = 1, 10 do
         table.insert(self.comets, {
-            x = math.random(WINDOW_WIDTH),
-            -- y = math.random(WINDOW_HEIGHT - 100)
+            x = math.random(-2,WINDOW_WIDTH),
+            y = -100,
+            dy = 0
         })
     end
+    self.x = math.random(WINDOW_WIDTH)
+    self.y = 100
+    
 end
 
 function Comet:update(dt)
+    for k, comet in pairs(self.comets) do
+        -- Acceleration
+        comet.dy = comet.dy + (math.random(-1,comet_speed) * dt)
+        comet.y = comet.y + comet.dy
+
+        --  self.y = self.y + (math.random(COMET_SPEED) * dt)
+        if comet.y > WINDOW_HEIGHT then
+            comet.dy = comet.dy -1
+            comet.y = - 100
+        end
+    end
 end
 
 function Comet:collides(target)
--- canon-game/states/PlayState.lua
+++ canon-game/states/PlayState.lua
@@ -1,15 +1,14 @@
 PlayState = Class{__includes = BaseState}
 
-
-
 function PlayState:enter()
---    self.bullets = Bullet()
    self.spacecraft = Spacecraft()
-   self.comets = Comet()
+    self.comets = Comet()
 end
 
 function PlayState:update(dt)
     self.spacecraft:update(dt)
+    self.comets:update(dt)
+
     if love.keyboard.wasPressed('escape') then
         love.event.quit()
     end
@@ -22,9 +21,9 @@ function PlayState:update(dt)
 end
 
 function PlayState:render()
-    love.graphics.setFont(gFonts['large'])
---    self.bullets:render()
-   self.comets:render()
-   self.spacecraft:render()
+    love.graphics.setFont(gFonts['large'])  
+    self.spacecraft:render()
+    self.comets:render()
+   
     love.graphics.printf({COLORS,'Playing the game'},12, 60, WINDOW_WIDTH, 'center')
 end
